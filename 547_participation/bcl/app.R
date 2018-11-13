55029d1d12a6c0f4eb8f06c4921b1993defbe6d2
Ray <rning.wu@gmail.com>
2018-11-13 10:47:25 -0800

    shiny

--- 547_participation/bcl/app.R
+++ 547_participation/bcl/app.R
@@ -8,15 +8,17 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel("This text is in the sidebar."),
     mainPanel(
-      "This text is in the main panel."
+      "This text is in the main panel.",
+      plotOutput("price_hist"),
+      tableOutput('table')
     )
   )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-
+  output$price_hist = renderPlot(ggplot2::qplot(bcl$Price))
+  output$table = renderTable(bcl)
 }
 
 # Run the application

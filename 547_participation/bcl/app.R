6e8bbdeca740b5d36e570674bb8ccd2ef8f0971c
Ray <rning.wu@gmail.com>
2018-11-13 10:27:57 -0800

    shiny

---
+++ 547_participation/bcl/app.R
@@ -0,0 +1,24 @@
+library(shiny)
+bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  titlePanel("BC Liquor price app",
+             windowTitle = "BCL app"),
+  sidebarLayout(
+    sidebarPanel("This text is in the sidebar."),
+    mainPanel(
+      "This text is in the main panel."
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
+
